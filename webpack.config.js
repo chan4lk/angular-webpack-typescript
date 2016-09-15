f56ee9af0d9511c02d79cd238cc65a51f65543a0
Chandima Ranaweera <chandima@chandimaranaweera.com>
2016-09-15 06:30:24 +0530

    build config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 module.exports = {
     entry: {
         app: './app',
-        vendor: ["jquery", "angular"]
+        vendor: ["jquery", "angular", "bootstrap"]
     },
     output: {
         filename: "dist/app.bundle.js"
